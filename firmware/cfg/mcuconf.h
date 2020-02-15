18fec08385af516d00be16b34ba8374136df7f72
Matthew Kennedy <matthewkennedy@outlook.com>
2020-02-14 21:32:13 -0800

    switch to hsi

--- firmware/cfg/mcuconf.h
+++ firmware/cfg/mcuconf.h
@@ -44,7 +44,7 @@
 #define STM32_LSI_ENABLED                   TRUE
 #define STM32_HSE_ENABLED                   FALSE
 #define STM32_LSE_ENABLED                   FALSE
-#define STM32_SW                            STM32_SW_PLL
+#define STM32_SW                            STM32_SW_HSI
 #define STM32_PLLSRC                        STM32_PLLSRC_HSI
 #define STM32_PREDIV_VALUE                  1
 #define STM32_PLLMUL_VALUE                  9
